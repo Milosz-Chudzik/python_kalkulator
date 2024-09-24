58b814bdd7bc04dda248d4e30839dd6a7b6bb062
Jonathan Morstad Cats <jonathanmcaats@gmail.com>
2024-09-24 12:50:50 +0200

    Update kalkulator_frontend.py

--- kalkulator_frontend.py
+++ kalkulator_frontend.py
@@ -1,6 +1,6 @@
 from back_end import *
 
-
+#printer en meny som vises på oppstart av kalkulatoren
 def printMeny():
     print("------------------- Kalkulator -------------------")
     print("| 1. Legg sammen (pluss)                         |")
@@ -11,6 +11,7 @@ def printMeny():
     print("--------------------------------------------------")
     menyvalg = input("Velg operasjon fra menyen: ")
     utfoerMenyvalg(menyvalg)
+# utfører en funksjon som ser hva brukeren har valgt fra menyen
 
 
 def utfoerMenyvalg(valgtTall):
@@ -24,10 +25,12 @@ def utfoerMenyvalg(valgtTall):
         gange()
     elif valgtTall == "4":
         unfinished()
+        # kjører funskjoner basert på hvilket tall som er valgt
     elif valgtTall == "5":
         bekreftelse = input("Er du sikker på at du vil avslutte? J/N ")
         if (bekreftelse == "J" or bekreftelse == "j" or bekreftelse == "Y" or bekreftelse == "y"):
             exit()
+            # bekrefter om du vil avslutte kalkulatoren
         else:
             printMeny()
     else:
@@ -35,6 +38,7 @@ def utfoerMenyvalg(valgtTall):
                             "Velg et tall mellom 1-4."
                             " Trykk for å fortsette *** ")
         printMeny()
+        # hvis den ikke ser et gyldig tall vil den reprinte menyen og si ugyldig valg
 
 
 def pause_og_fortsett():
