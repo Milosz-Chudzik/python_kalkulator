43ce7849696764ab0331d5f2aa6855225bb01519
Jonathan Morstad Cats <jonathanmcaats@gmail.com>
2024-09-24 13:27:08 +0200

    Update kalkulator_backend.py

--- kalkulator_backend.py
+++ kalkulator_backend.py
@@ -11,6 +11,12 @@ def trekkFra():
     diff = int(tall1) - int(tall2)
     print(f'{tall2} - {tall2} = {diff}')  #tar tall1 minus tall 2
 
+def ganging():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    sum = int(tall1) * int(tall2)
+    print(f'{tall2} * {tall2} = {sum}') #
+
 
 ''' Her er noen påbegynte tanker til utvidelse
 def gange():
